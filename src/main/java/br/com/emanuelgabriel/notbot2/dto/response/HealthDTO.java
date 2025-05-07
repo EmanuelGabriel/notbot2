c99a43c19b3224fcc60ca809e9154858554f5887
Emanuel Gabriel Sousa <emanuel.gabriel.sousa@hotmail.com>
2025-05-07 17:27:59 -0300

    feat: adicionando a exibição da versão e do profile escolhido'

--- src/main/java/br/com/emanuelgabriel/notbot2/dto/response/HealthDTO.java
+++ src/main/java/br/com/emanuelgabriel/notbot2/dto/response/HealthDTO.java
@@ -2,17 +2,24 @@ package br.com.emanuelgabriel.notbot2.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 
 @Data
 @AllArgsConstructor
 public class HealthDTO {
 
-    private String mensagem;
     private String status;
+    private String environment;
+    private String version;
+
+    public HealthDTO(Environment environment) {
+        this.status = HttpStatus.OK.toString();
+        this.environment = environment.getProperty("spring.profiles.active");
+        this.version = environment.getProperty("app.version");
+    }
 
     public HealthDTO() {
-        this.mensagem = "OK";
         this.status = HttpStatus.OK.toString();
     }
 
